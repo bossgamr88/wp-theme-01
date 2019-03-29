3608e452a7ff8244ce6adcde55f20913168e950c
pakawat <pakawatkut@gmail.com>
2019-03-30 03:56:51 +0700

    first-commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" href="<?php bloginfo('template_url')?>/seed.css" type="text/css">
+	<link rel="stylesheet" href="<?php bloginfo( 'template_url' )?>/style.css" type="text/css">
+	<title>เว็บทดสอบ</title>
+</head>
+
+<body>
+	<div id="page" class="site">
+		<header class="site-header">
+			<div class="site-branding">
+				<h1 class="site-title"><a href="#">เว็บทดสอบ</a></h1>
+			</div>
+			<nav class="site-navigation" role="navigation">
+				<div class="menu">
+					<ul>
+						<li><a href="#">About</a></li>
+					</ul>
+				</div>
+			</nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
+
+		<div class="site-content">
+			<div class="content-banner">
+				<img src="<?php  bloginfo( 'template_url' )?>/img/banner.jpg">
+			</div>
+
+			<div class="content-area">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis eros ex, quis pellentesque enim elementum vel. Etiam mollis at nisl eu fermentum. Aenean ut nisi sodales, finibus eros id, ultricies dui. Sed non fermentum ipsum. Donec ante ante, dapibus non urna a, mattis aliquet diam. Vivamus euismod vulputate ante, vitae consectetur purus blandit sed. Vestibulum nec justo volutpat, interdum ligula vel, tristique tortor. Nullam vel urna metus. Nunc maximus posuere lacus vitae maximus.</p>
+
+				<p>Vivamus eu dolor consequat, ullamcorper nisi eu, viverra dui. Pellentesque pretium condimentum bibendum. Nunc condimentum sem eget consequat malesuada. Aliquam porta volutpat consectetur. Morbi elementum semper efficitur. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum in ante quam. Integer faucibus blandit neque, at laoreet odio varius sit amet. In rutrum orci quis orci sollicitudin, eu suscipit est ornare. Fusce vitae neque nulla.</p>
+
+				<p>Etiam elementum, felis ac suscipit maximus, tortor nibh elementum velit, consectetur ornare nibh tellus id est. Etiam elit arcu, finibus vitae augue eget, tincidunt maximus mi. Suspendisse finibus, nunc vitae euismod maximus, tellus mauris lobortis arcu, commodo viverra quam diam a velit. Mauris sapien ex, pulvinar id suscipit ut, dapibus sit amet erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. In feugiat dui id eros sollicitudin facilisis ut ac arcu. Ut pulvinar egestas convallis. Mauris in orci diam. Vestibulum tortor ex, dictum ac tempus in, lobortis non nunc. Ut quam libero, sollicitudin nec enim vel, consequat blandit massa. Vestibulum nibh purus, consectetur a feugiat sed, ultricies a lectus. Nulla sollicitudin ligula a tortor euismod lacinia. Nulla pharetra, leo et ornare tristique, sapien leo volutpat nisi, at laoreet lectus sem sed urna. Nullam dapibus risus a erat tincidunt maximus. Duis vel lorem finibus, elementum ligula a, ornare ex.</p>
+
+			</div>
+
+			<aside class="widget-area">
+				<aside class="widget">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis eros ex, quis pellentesque enim elementum vel.
+				</aside>
+				<aside class="widget">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis eros ex, quis pellentesque enim elementum vel.
+				</aside>
+				<aside class="widget">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis eros ex, quis pellentesque enim elementum vel.
+				</aside>
+			</aside>
+		</div><!-- #content -->
+
+		<footer class="site-footer">
+			<div class="site-info">© 2016 เว็บทดสอบ</div>
+			<div class="site-credit">Theme by <a href="http://seedthemes.com/" target="_blank">SeedThemes</a></div>
+		</footer><!-- site-footer -->
+	</div><!-- #page -->
+</body>
+</html>
