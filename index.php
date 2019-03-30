35c29cb6e6092ce5ed1785c4e485bd0e19b922e4
pakawat <pakawatkut@gmail.com>
2019-03-30 21:07:42 +0700

    change name_file

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header();?>
 			<div class="content-area">
 					<?php while (have_posts()) : the_post()  ?>
-						<h1><?php the_title() ?></h1>	
+						<h1><a  href="<?php the_permalink() ?>"> <?php the_title() ?></a></h1>	
 							<?php the_content() ?>
 					<?php endwhile ?>
 			</div>
